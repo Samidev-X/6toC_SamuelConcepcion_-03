83af57b17c4998ee93220529d163ba3dd240ae05
Samuel Concepción <161236888+Samidev-X@users.noreply.github.com>
2024-03-20 18:38:46 -0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,105 @@
+function alerta() {
+    alert("Para una mejor experiencia active 'PANTALLA COMPLETA (F11)'");
+    alert("Las Páginas cargarán mas rapido dependiendo de la velocidad de su ancho de banda")
+}
+
+    window.onload = alerta
+
+function notAvable() {
+    alert("Esta Opcion por el momento no está Disponible");
+}
+
+function notAvablePapelera() {
+    alert("No hay nada en la papelera")
+}
+
+///
+function mostrar() {
+    document.getElementById('page').style.display = 'block';
+}
+
+function cerrar() {
+    document.getElementById('page').style.display = 'none';
+}
+///
+///
+function mostrar1() {
+    document.getElementById('page1').style.display = 'block';
+}
+
+function cerrar1() {
+    document.getElementById('page1').style.display = 'none';
+}
+///
+///
+function mostrar2() {
+    document.getElementById('page2').style.display = 'block';
+}
+
+function cerrar2() {
+    document.getElementById('page2').style.display = 'none';
+}
+///
+///
+function mostrar3() {
+    document.getElementById('page3').style.display = 'block';
+}
+
+function cerrar3() {
+    document.getElementById('page3').style.display = 'none';
+}
+///
+///
+function mostrar4() {
+    document.getElementById('page4').style.display = 'block';
+}
+
+function cerrar4() {
+    document.getElementById('page4').style.display = 'none';
+}
+///
+///
+function mostrar5() {
+    document.getElementById('page5').style.display = 'block';
+}
+
+function cerrar5() {
+    document.getElementById('page5').style.display = 'none';
+}
+///
+///
+function mostrar6() {
+    document.getElementById('page6').style.display = 'block';
+}
+
+function cerrar6() {
+    document.getElementById('page6').style.display = 'none';
+}
+///
+///
+function mostrar7() {
+    document.getElementById('page7').style.display = 'block';
+}
+
+function cerrar7() {
+    document.getElementById('page7').style.display = 'none';
+}
+
+///
+///
+function mostrar8() {
+    document.getElementById('page8').style.display = 'block';
+}
+
+function cerrar8() {
+    document.getElementById('page8').style.display = 'none';
+}
+///
+///
+function mostrar9() {
+    document.getElementById('page9').style.display = 'block';
+}
+
+function cerrar9() {
+    document.getElementById('page9').style.display = 'none';
+}
